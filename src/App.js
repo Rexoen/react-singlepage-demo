7a735136999ea32a5256202a13405d4d3354a05b
RexGuo <rex.guo@luxshare-ict.com>
2021-11-14 15:36:49 +0800

    helloworld and todo demo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+    state = {
+        val : '',
+        list:[]
+    }
+    handleChange = (event) => {
+        let val = event.target.value;
+        this.setState({
+            val
+        })
+        }
+    handleAdd = () => {
+        let {val,list} = this.state;
+        list.push(val)
+        this.setState({
+            list
+    })
+    }
+
+    render(){
+      const {val,list} = this.state;
+        return <div>
+        <p>This is my first line of codes in react!</p>
+        <h1>Hello World!</h1>
+        <input type="text" value={val} onChange={this.handleChange}/>
+        <button onClick={this.handleAdd}>Add</button>
+        <ul>
+            {
+                list.map((item,index)=>{
+                    return <li key={index}>{item}</li>
+                })
+            }
+        </ul>
+
+      </div>
+
+    }
 }
+
+// 官方默认生成的写法(函数式)
+//function App() {
+//  return (
+//      <div>
+//        <p>This is my first line of codes in react!</p>
+//        <h1>Hello World!</h1>
+//        <input type=text onChange={}>
+//      </div>
+//  );
+//}
 
 export default App;
